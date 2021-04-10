275a6494ce5ed0f82a0a6e11ab69e2d341b80853
m2vi <iuvo@gmx.at>
2021-04-10 14:16:43 +0200

    init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,23 +38,6 @@ class MyDocument extends Document {
           <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="620" />
-
-          <meta property="twitter:site" content="github" />
-          <meta property="twitter:site:id" content="13334762" />
-          <meta property="twitter:creator" content="github" />
-          <meta property="twitter:creator:id" content="13334762" />
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:title" content="GitHub" />
-          <meta
-            property="twitter:description"
-            content="GitHub is where people build software. More than 56 million people use GitHub to discover, fork, and contribute to over 100 million projects."
-          />
-          <meta
-            property="twitter:image:src"
-            content="https://github.githubassets.com/images/modules/open_graph/github-logo.png"
-          />
-          <meta property="twitter:image:width" content="1200" />
-          <meta property="twitter:image:height" content="1200" />
         </Head>
         <body>
           <Main />
