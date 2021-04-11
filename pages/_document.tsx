c4a3c38f858d2911c062f6da7ad54ad69fd552f0
m2vi <iuvo@gmx.at>
2021-04-11 14:21:04 +0200

    init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,8 +10,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en-GB">
         <Head>
-          <meta property="og:url" content="https://github.com" />
-          <meta property="og:site_name" content="developed by m2v" />
+          <meta property="og:url" content="https://collector-magnificus.vercel.app/" />
+          <meta property="og:site_name" content="collector-magnificus.vercel.app" />
           <meta property="og:title" content="Collector Magnificus" />
           <meta
             property="og:description"
@@ -29,6 +29,36 @@ class MyDocument extends Document {
           <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="1200" />
+          <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#0d1014"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#0d1014" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#0d1014" />
         </Head>
         <body>
           <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,11 @@ import {
 } from "react-notifications";
 
 import dataToSend from "../utils/dataToSend";
+import Loader from "../components/loader";
 
 import useSWR from "swr";
 import fetcher from "../utils/fetcher";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 declare global {
   interface Window {
@@ -18,6 +19,7 @@ declare global {
   }
 }
 export default function Home() {
+  const [success, setSuccess] = useState(false);
   const [loadingClass, setLoadingClass] = useState("not-loading");
   let { data, error } = useSWR("https://api.ipify.org/?format=json", fetcher);
 
@@ -37,11 +39,15 @@ export default function Home() {
     })
       .then((res) => res.json())
       .then((res) => {
-        onSuccess(res)
+        onSuccess(res);
         setLoadingClass("not-loading");
-      }).catch((err) => {
-        onError(err)
+        setSuccess(true);
       })
+      .catch((err) => {
+        onError(err);
+        setLoadingClass("not-loading");
+        setSuccess(false);
+      });
   };
 
   const onError = async (error: any, message?: string) => {
@@ -50,7 +56,8 @@ export default function Home() {
       time: new Date(),
     });
     NotificationManager.error(
-      (message || "It's a pity, there's been an error. If you want to know more, take a look at the console."),
+      message ||
+        "It's a pity, there's been an error. If you want to know more, take a look at the console.",
       <>
         <img src="/icons/error.svg" alt="Error"></img>
       </>
@@ -58,82 +65,27 @@ export default function Home() {
   };
 
   const onSuccess = async (data: any, message?: string) => {
-    console.log(data)
-    NotificationManager.success (
-      (message || "Data has been sent. Thank you for participating!"),
+    console.log(data);
+    NotificationManager.success(
+      message || "Data has been sent. Thank you for participating!",
       <>
         <img src="/icons/success.svg" alt="Success" width="24"></img>
       </>
-    )
+    );
   };
 
   return (
     <div className="flex justify-center items-center min-h-100">
       <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#0d1014"
-        />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#0d1014" />
-        <meta
-          name="msapplication-config"
-          content="/favicon/browserconfig.xml"
-        />
-        <meta name="theme-color" content="#0d1014" />
-
-        <title>Home</title>
+        <title>Launch</title>
       </Head>
-      <button className={`btn primary ${loadingClass}`} onClick={sendData}>
+      <button
+        className={`btn primary ${loadingClass}`}
+        onClick={sendData}
+        disabled={success}
+      >
         <span>Launch</span>
-        <div className="loader">
-          <svg
-            version="1.1"
-            id="loader-1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            width="24px"
-            height="24px"
-            viewBox="0 0 50 50"
-            xmlSpace="preserve"
-          >
-            <path
-              fill="#fff"
-              d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"
-            >
-              <animateTransform
-                attributeType="xml"
-                attributeName="transform"
-                type="rotate"
-                from="0 25 25"
-                to="360 25 25"
-                dur="1s"
-                repeatCount="indefinite"
-              />
-            </path>
-          </svg>
-        </div>
+        <Loader />
       </button>
       <NotificationContainer />
     </div>
